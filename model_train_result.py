5723573d29ec8b50e1f5791967188758d4099349
sooox0809 <skycasey@inha.edu>
2023-08-12 16:18:31 +0900

    [AI-weapon detection] Make CSV file for model test (ver.background subtraction)

--- model_train_result.py
+++ model_train_result.py
@@ -1,3 +1,5 @@
+## 이미지 저장 할 필요 x 영상에서 바로 모델 -> 결과 이미지를 저장하는 걸로 변경
+
 import cv2
 import numpy as np
 import torch
@@ -10,18 +12,18 @@ from pandas import DataFrame, Series
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = YOLO("best.pt")  # 여기 적용할 모델
+model = YOLO("/Users/sooox89/Desktop/workspace/pythonProject/computervision/I_CU/best_custom.pt")  # 여기 적용할 모델
+
+#박스 환경설정
+box_annotator = sv.BoxAnnotator(thickness=2, text_thickness=2, text_scale=1)
 
 cnt = 0
-
-# class에 맞게 수정
 list_filename = list()
 list_classname = list()
 list_confidence = list()
 list_fist = list()
 list_hammer = list()
 list_knife = list()
-
 
 def get_files_in_folder(folder_path):
     file_list = []
@@ -32,28 +34,76 @@ def get_files_in_folder(folder_path):
     return file_list
 
 
-folder_path = ''  # 영상 폴더 지정
+folder_path = '/Users/sooox89/Desktop/workspace/pythonProject/computervision/I_CU/real_test_video'  # 영상 폴더 지정
 files = get_files_in_folder(folder_path)
 count_frame= []
 for file in files:
     file = str(file)
+    output_path = './'+ file +'/ex/ex_mask_background_subtraction_output.mp4'
+    file = ("/Users/sooox89/Desktop/workspace/pythonProject/computervision/I_CU/real_test_video/" + file)  # 영상 폴더 지정
     print(file)
-    file = ('' + file)  # 영상 폴더 지정
 
-    cap = cv2.VideoCapture(file)
+    # 영상 불러오기, 출력 영상 설정
+    video_path = file
 
-    box_annotator = sv.BoxAnnotator(thickness=2, text_thickness=2, text_scale=1)
+    cap = cv2.VideoCapture(video_path)
+
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    codec = cv2.VideoWriter_fourcc(*'mp4v') # 출력 비디오 코덱
+
+    # 영상 크기 설정
+    out = cv2.VideoWriter(output_path, codec, fps, (width, height))
+
+    # 배경 제거를 위해 객체 생성 및 커널 설정
+    # fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False) # detectShadows=False : 그림자 감지 안함
+    fgbg = cv2.createBackgroundSubtractorKNN(detectShadows=False) # detectShadows=False : 그림자 감지 안함
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)) # 배경 제거 결과에 적용할 모폴로지 연산 커널 설정
+
+    frame_num = 0
+
+    # 창 생성
+    cv2.namedWindow('background extraction video', cv2.WINDOW_NORMAL)
 
     while cap.isOpened():
         ret, img = cap.read()
         if not ret:
             break
+
         results = model.predict(img)[0]
 
-        detections = sv.Detections.from_yolov8(results)
-        high_confidence_detections = detections[detections.confidence > 0.5]
+        frame_num += 1
 
-        img = box_annotator.annotate(scene=img, detections=high_confidence_detections)
+        # 프레임 크기 절반으로 축소
+        resized_img = cv2.resize(img, None, fx=0.5, fy=0.5)
+
+        # 배경 제거
+        background_extraction_mask = fgbg.apply(resized_img)
+        background_extraction_mask = cv2.dilate(background_extraction_mask, kernel, iterations=1) # cv2.dilate : 배경제거 마스크 확장
+
+        # 크기 복원
+        background_extraction_mask = cv2.resize(background_extraction_mask, (width, height))
+
+        background_extraction_mask = np.stack((background_extraction_mask,) * 3, axis=-1)
+        bitwise_image = cv2.bitwise_and(img, background_extraction_mask)
+
+        # 창 크기 줄이기
+        window_width = int(width * 0.7)
+        window_height = int(height * 0.7)
+        cv2.resizeWindow('background extraction video', window_width, window_height)
+
+        # 배경이 제거된 프레임만 저장
+        out.write(bitwise_image)  # 수정: 배경이 제거된 bitwise_image를 저장
+
+        results = model.predict(bitwise_image)[0]
+
+        classes = ["fist", "hammer", "knife"]
+        detections = sv.Detections.from_yolov8(results)
+        high_confidence_detections = detections[detections.confidence >= 0.5]
+        labels = [f"{classes[class_id]} {confidence:0.2f}" for _, _, confidence, class_id, _ in high_confidence_detections]
+        bitwise_image = box_annotator.annotate(scene=bitwise_image, detections=high_confidence_detections, labels=labels)
 
         fist = 0
         hammer = 0
@@ -79,17 +129,21 @@ for file in files:
             list_filename.append(str("frame%d" % cnt))
             list_confidence.append(str(high_confidence_detections.confidence)[1:-1])
 
-            cv2.imwrite(""+"/frame%d.png" % cnt, img)  # 앞부분에 결과이미지를 저장할 주소 적어주시면 됩니다.
+            cv2.imwrite("/Users/sooox89/Desktop/workspace/pythonProject/computervision/I_CU/result/best_custom_back/frame%d.png" % cnt, bitwise_image)  # 여기 앞부분에 결과이미지를 저장할 주소 적어주시면 됩니다.
             print('Saved frame number :', str(int(cap.get(1))//15))
             cnt += 1
     count_frame.append(cnt)
 
-# class에 맞게 수정
-data = {'filename': list_filename, 'fist': list_fist,'hammer': list_hammer,'knife': list_knife, 'conf': list_confidence}
+
+
+data = {'filename': list_filename,
+        'fist': list_fist,
+        'hammer': list_hammer,
+        'knife': list_knife,
+        'conf': list_confidence}
 
 test_result = pd.DataFrame(data)
 
 print(test_result.shape)
-print('hammer:',count_frame[1],'knife:',count_frame[3],'buying:',count_frame[11],'fist:',count_frame[13])
 
-test_result.to_csv('best.csv', header=True, index=True)  # 여기 csv 저장할 파일 경로와 저장명을 지정해주세요
+test_result.to_csv('/Users/sooox89/Desktop/workspace/pythonProject/computervision/I_CU/result/best_custom_back.csv', header=True, index=True)  # 여기 csv 저장할 파일 경로와 저장명을 지정해주세요
